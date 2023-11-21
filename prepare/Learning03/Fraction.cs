a2664e168fe686cca7951f936b8b1d7e97c8c1a6
gino004 <gino2137@hotmail.com>
2023-11-20 22:00:01 -0500

    tarea3

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,45 @@
+using System;
+
+public class Fraction{
+
+ private int _top;
+ private int _bottom;
+
+
+ public Fraction(){
+   //inicializa por defecto 1/1
+    _top = 1;
+    _bottom = 1;
+ }
+
+ public Fraction(int wholeNumber){
+    // el wholeNumber es donde ira arriba de la fraccion, cuando se de un numero cualquiera
+    // seria x/1(fraccion)
+   _top = wholeNumber;
+   _bottom = 1;
+
+
+ }
+
+ public Fraction(int top, int bottom){
+
+     _top = top;
+     _bottom = bottom;
+
+ }
+
+ public string GetFractionString(){
+
+    string text = $"{_top}/{_bottom}";
+    return text;
+ }
+
+ public double GetDecimalValue(){
+
+    return (double)_top / (double)_bottom;
+
+ }
+  
+
+
+}
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,55 +1,58 @@
 using System;
-using System.IO; 
+using System.IO;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    PromptGenerator pg = new PromptGenerator();
+    Journal journal = new Journal();
+    string pro_ = pg.GetRandomPrompt();
+    string _no;
+
+    _no = Display_01();
+
+    if (_no == "1")
     {
-     PromptGenerator pg = new PromptGenerator();
-     Journal journal = new Journal();
-     string pro_ = pg.GetRandomPrompt();
-     string _no;
 
-     _no = Display_01();
+      Console.WriteLine($"{pro_}");
+      _no = Console.ReadLine();
 
-     if ( _no == "1"){
-        
-        Console.WriteLine($"{pro_}");
-        _no = Console.ReadLine();
-
-        Entry  en = new Entry();
-        en._date = DateTime.Now.ToString();
-        en._entryText = _no;
-        en._promptText = pro_;
-        journal.AddEntry(en);
-     }
-      
-      if(_no == "2"){
-
-        journal.DisplayAll();
-
-
-
-      }
-     
-     
+      Entry en = new Entry();
+      en._date = DateTime.Now.ToString();
+      en._entryText = _no;
+      en._promptText = pro_;
+      journal.AddEntry(en);
     }
-    
 
-    static string Display_01(){
+    if (_no == "2")
+    {
 
-     string display;   
+      journal.DisplayAll();
 
-     Console.WriteLine("Write 1");
-     Console.WriteLine("Display 2");
-     Console.WriteLine("Load 3");
-     Console.WriteLine("Save 4");
-     Console.WriteLine("Quit 5");
 
-     return display = Console.ReadLine();
 
-     
     }
+
+
+  }
+
+
+  static string Display_01()
+  {
+
+    string display;
+
+    Console.WriteLine("Write 1");
+    Console.WriteLine("Display 2");
+    Console.WriteLine("Load 3");
+    Console.WriteLine("Save 4");
+    Console.WriteLine("Quit 5");
+
+    return display = Console.ReadLine();
+
+
+  }
 
 
 }
