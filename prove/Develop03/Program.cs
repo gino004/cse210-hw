f31b8fafa5f4b355c49f9f3cd43570431f7905bd
gino004 <gino2137@hotmail.com>
2023-11-23 21:11:38 -0500

    reciente

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -7,11 +7,11 @@ class Program
     static void Main(string[] args)
     {
         Reference re = new Reference("Isaías", 53, 2);
-        Scripture sc = new Scripture(re,"Porque subíra cual renuevo delante de él, y como raíz de tierra seca; no hay parecer en él ni hermosura; y cuando le yeamos, no habrá en el atractivo para que le deseemos");
-      //Scripture sc = new Scripture(re,"uno dos tres");
+        Scripture sc = new Scripture(re, "Porque subíra cual renuevo delante de él, y como raíz de tierra seca; no hay parecer en él ni hermosura; y cuando le yeamos, no habrá en el atractivo para que le deseemos");
+        //Scripture sc = new Scripture(re,"uno dos tres");
 
         // string quit = "";
-        
+
         // while(sc.IscompletelyHidden() == false || quit != "Finish"){
 
         //     Console.WriteLine(sc.GetDisplayText());
@@ -19,22 +19,25 @@ class Program
         //     sc.HideRandomWords(1);
         // }
 
-           bool keep = true; 
-           string quit = "";
- 
-         while(keep == true){
-           Console.WriteLine(sc.GetDisplayText());
-              quit = Console.ReadLine();
-              sc.HideRandomWords(5);
+        bool keep = true;
+        string quit = "";
 
-              if(sc.IscompletelyHidden()){
+        while (keep == true)
+        {
+            Console.WriteLine(sc.GetDisplayText());
+            quit = Console.ReadLine();
+            sc.HideRandomWords(5);
+
+            if (sc.IscompletelyHidden())
+            {
                 keep = false;
             }
-              if(quit == "Finish"){
+            if (quit == "Finish")
+            {
                 keep = false;
             }
 
-}
+        }
 
 
     }
